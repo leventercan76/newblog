405a437193e4a6fae331ad0fbfa68b24f8b720ad
leventercan76 <116818895+leventercan76@users.noreply.github.com>
2024-01-02 09:51:58 +0300

    02/01/2023

--- user/views.py
+++ user/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterForm
+from .forms import LoginForm 
 
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate,logout
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
+    form = RegisterForm(request.POST or None )
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")       
+
+        newUser = User(username=username)
+        newUser.set_password(password)
+
+        newUser.save()
+        login(request,newUser)
+        messages.success(request,"Kayıt başarılı")
+
+        return redirect("index")
+    else:
+        context = {
+            "form" : form
+        }
+    return render(request,"register.html",context)
+    """ zahmetli ve karışık daha iyisi var
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -30,10 +51,29 @@ def register(request):
         context = {
             "form" : form
         }
-        return render(request,"register.html",context)
+        return render(request,"register.html",context) """
 
 def loginUser(request):
-    return render(request,"login.html")
+    form = LoginForm(request.POST or None )
+    context = {
+            "form" : form
+        }
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")       
+        user = authenticate(username=username,password=password)
+        
+        if user is None:
+            messages.info(request,"Kullanıcı Adı yada Parola hatalı")
+            return render(request,"login.html",context)
+        else:
+            messages.success(request,"Login başarılı")
+            login(request,user=user)
+            return redirect("index")
+    
+        
+    return render(request,"login.html",context)
 
 def logoutUser(request):
+    logout(request)
     return render(request,"logout.html")
